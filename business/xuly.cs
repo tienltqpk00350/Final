3a1cb4d6df742db36c57beb6772a46e0a0677d0d
tienltqpk00350 <tienltqpk00350@gmail.com>
2016-07-04 09:07:14 +0700

    P

---
+++ business/xuly.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Final.dataConfig;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Final.business
+{
+    public class xuly
+    {
+        public int chuyen;
+        core khoitao = new core();
+        public DataTable loadlop = new DataTable();
+        public DataTable khoitao2 = new DataTable();
+
+        //login
+        public int login(string user, string pass)
+        {
+            khoitao2 = khoitao.bang(@"select * from login where taikhoan='" + user + "' and matkhau='" + pass + "'");
+            if (khoitao2.Rows.Count == 1)
+            {
+                chuyen = Convert.ToInt32(khoitao2.Rows[0]["quyen"].ToString());
+            }
+            else
+                MessageBox.Show("Thất Bại");
+            return chuyen;
+        }
+        //
+
+
+
+
+
+
+
+
+
+
+
+
+        //####################load###################################
+        //loadbienbanthi
+        public DataTable loadview()
+        {
+            khoitao2 = khoitao.bang(@"select TenMH,MaSP,Tensp from QLMH left join QLSP on QLMH.MA=QLSP.MA_QLMH order by TenMH asc");
+            return khoitao2;
+        }
+        //loadMathang
+        public DataTable loadmh()
+        {
+            khoitao2 = khoitao.bang(@"select * from QLMH");
+            return khoitao2;
+        }
+        public DataTable loadsp()
+        {
+            khoitao2 = khoitao.bang(@"select MaSP,Tensp,ngaynhap,ngaysuat,soluong,donvitinh,dongia,ghichu from QLSP");
+            return khoitao2;
+        }
+        //timkiemallinone
+        public DataTable TimKiemAllInOne(string chuoi)
+        {
+            khoitao2 = khoitao.bang(@"" + chuoi + "");
+            return khoitao2;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //####################################checktrung#####################
+        public bool checktrung(string checktrung)
+        {
+            khoitao2 = khoitao.bang(@"");
+            if (khoitao2.Rows.Count > 0)
+            {
+                MessageBox.Show("Mã Giảng Viên Đã Tồn Tại");
+                return false;
+            }
+            return true;
+        }
+        //kiemtratenmon
+        public bool kiemtratenmon(string tenmon)
+        {
+            khoitao2 = khoitao.bang(@"select * from MonHoc where TenMonHoc=N'" + tenmon + "'");
+            if (khoitao2.Rows.Count == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        //#####################################sua############################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region ham edit sp
+        public void suasp(string tensp,string ngaynhap,string ngaysuat,string soluong,string donvitinh,string dongia,string ghichu,string ma,string thaydoi)
+        {
+            try
+            {
+                khoitao.themsoasua(@"update QLSP set Tensp='" + tensp + "',ngaynhap=convert(datetime, '" + ngaynhap + "', 103),ngaysuat=convert(datetime, '" + ngaysuat + "', 103),soluong='" + soluong + "',donvitinh='" + donvitinh + "',dongia='" + dongia + "',ghichu='" + ghichu + "',MA_QLMH='" + ma + "' where MaSP='"+thaydoi+"'");
+                MessageBox.Show(@"đã Sửa  " + khoitao.travebanghidcthuchien + " bản ghi");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Sửa không hoàn hành        :" + Environment.NewLine + ex.Message.ToString(), "Lỗi Không Hoành Thành", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        #endregion
+
+
+
+        #region Ham them SP
+        public void Themsp(string MaSP, string Tensp, string ngaynhap, string ngaysuat, string soluong, string donvitinh, string dongia, string ghichu, string MA_QLMH)
+        {
+            try
+            {
+                khoitao.themsoasua(@"insert into QLSP (MaSP,Tensp,ngaynhap,ngaysuat,soluong,donvitinh,dongia,ghichu,MA_QLMH) values ('" + MaSP + "','" + Tensp + "',convert(datetime, '" + ngaynhap + "', 103),convert(datetime, '" + ngaysuat + "', 103),'" + soluong + "','" + donvitinh + "','" + dongia + "','" + ghichu + "','" + MA_QLMH + "')");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Mã Đã Tồn Tại        :" + Environment.NewLine + ex.Message.ToString(), "Lỗi Không Hoành Thành", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        #endregion
+
+
+
+
+        #region Ham Xoa San Pham Su Dung SP
+        public void delsp(string ID)
+        {
+            try
+            {
+                khoitao.hamdelSP(@"delSP", ID);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("thêm không hoàn hành        :" + Environment.NewLine + ex.Message.ToString(), "Lỗi Không Hoành Thành", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        #endregion
+    }
+}
---
+++ form/view.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Final.form
+{
+    partial class view
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(773, 338);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "TenMH";
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(89)))), ((int)(((byte)(89)))));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "Tên Mặt Hàng";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "MaSP";
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(133)))), ((int)(((byte)(97)))));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "Mã Sản Phẩm";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "Tensp";
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(133)))), ((int)(((byte)(97)))));
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column3.HeaderText = "Tên Sản Phẩm";
+            this.Column3.Name = "Column3";
+            // 
+            // view
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "view";
+            this.Size = new System.Drawing.Size(773, 338);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
